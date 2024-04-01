66b12433ecd3ab6842564e3d6ec2eb9dc9155e06
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-04-02 02:29:47 +0600

    animation done

--- main.js
+++ main.js
@@ -7,5 +7,17 @@ if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    // console.log(scrollerContent);
+
+    scrollerContent.forEach((content) => {
+      const duplicatedItem = content.cloneNode(true);
+      //   scrollerInner.appendChild(clone);
+      duplicatedItem.setAttribute("area-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+      console.log(duplicatedItem);
+    });
   });
 }
